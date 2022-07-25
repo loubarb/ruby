72dd77f3f5151ca2636f1c0fdc038b59c10528ed
loubarb <louis.barbati@gmail.com>
2022-07-24 22:15:05 -0400

    Implement methods

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -1,3 +1,8 @@
+def always_three(number)
+  puts "Always " + (((number + 5) * 2 - 4) / 2 - number).to_s
+end
+
 puts "Give me a number"
 user_number = gets.to_i
-puts "Always " + (((user_number + 5) * 2 - 4) / 2 - user_number).to_s
+
+always_three(user_number)
---
+++ ruby_challenges/methods.rb
@@ -0,0 +1,12 @@
+def my_first_method
+  puts "This is my first method!"
+end
+
+my_first_method
+
+def math_method(num1, num2)
+  sum = num1 + num2
+  puts sum
+end
+
+math_method(2, 9)
