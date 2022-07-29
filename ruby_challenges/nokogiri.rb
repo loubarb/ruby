9e500fa5e5ce011c12da9f8ca238e8a8706f273b
loubarb <louis.barbati@gmail.com>
2022-07-29 15:42:13 -0400

    Nokogiri gem

--- ruby_challenges/nokogiri.rb
+++ ruby_challenges/nokogiri.rb
@@ -4,3 +4,9 @@ require 'open-uri'
 doc = Nokogiri::HTML(URI.open("https://www.sainsburys.co.uk/gol-ui/recipes/italian-pressed-sandwich"))
 
 puts doc.css('ul')
+
+list = doc.css('.ln-o-bare-list')
+
+list.each do |ingridients|
+  puts ingridients.inner_html
+end
